5e3c7ba80d9b6beea3acd7a6ef8be899d5a0d370
Deepesh Bhardwaj <70226498+deepesh16b@users.noreply.github.com>
2024-03-24 12:48:55 +0530

    Update HeroSection.jsx

--- src/components/Homepage/HeroSection.jsx
+++ src/components/Homepage/HeroSection.jsx
@@ -33,7 +33,7 @@ export const HeroSection = () => {
             </svg>
           </a>
           
-          <Link href="/sign" className="inline-flex items-center justify-center  py-3 w-36 text-base font-medium text-center text-gray-900 border border-blue-600 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100" >Sign In</Link>
+          <Link href="/signin" className="inline-flex items-center justify-center  py-3 w-36 text-base font-medium text-center text-gray-900 border border-blue-600 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100" >Sign In</Link>
           
         </div>
         <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
